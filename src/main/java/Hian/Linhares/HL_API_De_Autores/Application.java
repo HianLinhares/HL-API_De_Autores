5b6a7c886d60c594af614c4ba6ef6c182934267b
Hian Linhares <hianfernando@hotmail.com>
2025-09-29 18:06:44 -0300

    feat: primeiro commit da API de autores

--- src/main/java/Hian/Linhares/HL_API_De_Autores/Application.java
+++ src/main/java/Hian/Linhares/HL_API_De_Autores/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HlApiDeAutoresApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HlApiDeAutoresApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
