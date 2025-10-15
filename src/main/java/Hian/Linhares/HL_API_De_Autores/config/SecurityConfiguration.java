3a0e53670ebeb5666822b7e74400b41428ac1de9
Hian Linhares <hianfernando@hotmail.com>
2025-10-14 22:59:17 -0300

    feat: Adicionando as configurações de segurança na API

---
+++ src/main/java/Hian/Linhares/HL_API_De_Autores/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package Hian.Linhares.HL_API_De_Autores.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable) //habilitando o acesso de outras aplicações a essa API
+                .formLogin(configurer-> configurer.loginPage("/login.html")) //criando o próprio formulário de login para autenticação
+                .httpBasic(Customizer.withDefaults()) //habilitando http basic (autenticação através do chrome ou do postman)
+                .authorizeHttpRequests(authorize-> authorize.anyRequest().authenticated()) //definindo que todas requisições a API precisam estar autenticadas
+                .build();
+    }
+
+}
